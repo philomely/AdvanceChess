6ecd8f122a99678fae269d57fa7ff5656eaf61f1
aaron.yang <ziniu19@hotmail.com>
2022-06-03 18:25:35 +0100

    things

--- server/game/unit/baseunit.lua
+++ server/game/unit/baseunit.lua
@@ -1,7 +1,17 @@
 local BaseUnit = class("BaseUnit")
 
+function BaseUnit:initialize()
+    self.x = 0
+    self.y = 0
+end
+
 function BaseUnit:getName()
     return "baseunit"
+end
+
+function BaseUnit:moveToPos(int x, int y)
+    self.x = x
+    self.y = y
 end
 
 return BaseUnit
