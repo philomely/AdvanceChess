433bb9dbd596b8532092972d4cca92dd19eade06
aaron yang <ziniu19@hotmail.com>
2024-05-01 20:58:07 +0100

    needs

--- server/grand/grandgame.lua
+++ server/grand/grandgame.lua
@@ -1,10 +1,13 @@
+require("base/constants")
 local GrandGame = class('GrandGame')
+local People = require('people.people')
 function GrandGame:initialize()
-
+    self.people = People:new()
 end
 
 function GrandGame:tick()
     print("GrandGame tick")
+    self.people:tick()
     return 0
 end
 
--- server/grand/people/people.lua
+++ server/grand/people/people.lua
@@ -1,11 +1,20 @@
+require("../base/constants")
 local Poeple = class('Poeple')
 function Poeple:initialize()
     self.population = 0
     self.happiness = 0
     self.wellbeing = 0
+    self.needs = {
+        [NEED_AIR] = 10,
+        [NEED_WATER] = 20
+    }
 end
 
 function Poeple:tick()
+    for k,v in pairs(self.needs) do
+        print(k.." = "..v)
+    end
+    print(NEED_AIR)
 
 end
 
